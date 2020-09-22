f6cce50dd3d196d8975b7911e52606359ad2f00f
Jim S <jims@rutgers.edu>
2020-09-22 15:51:00 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,2 @@
 node default { 
-  file {'/root/README':
-     ensure => file,
-    content => 'This is a README file',
-      owner => 'root',
-  }
-  file {'/root/README':
-    owner => 'root',
-  }
 }
